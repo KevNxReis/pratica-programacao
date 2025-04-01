a3abfd5dfb5863f1af42cbd2827120702f30904c
Kevin Reis <kevinreis747@gmail.com>
2025-04-01 11:00:02 -0300

    Atualização automática - 2025-04-01 11:00:02

---
+++ java/maratona-java/src/estudojava/pratica/Ola2Java.java
@@ -0,0 +1,9 @@
+package estudojava.pratica;
+
+public class Ola2Java {
+    public static void main(String[] args) {
+        // int, double, float, char, boolean, byte, short, long
+        int idade = 10;
+        System.out.println("O valor da idade é " + idade);
+    }
+}
