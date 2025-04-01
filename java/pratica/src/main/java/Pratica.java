555a99a06ca1c2c383fba9a9de8050b21ab11311
Kevin Reis <kevinreis747@gmail.com>
2025-03-31 22:00:02 -0300

    Atualização automática - 2025-03-31 22:00:02

--- java/pratica/src/main/java/Pratica.java
+++
@@ -1,5 +0,0 @@
-public class Pratica {
-    public static void main(String[] args) {
-        System.out.println("Kamehamehaaaa");
-    }
-}
---
+++ pratica/src/test/java/estudo/AppTest.java
@@ -0,0 +1,12 @@
+package estudo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testExample() {
+        assertTrue(1 + 1 == 2, "Soma simples deve ser 2");
+    }
+}
