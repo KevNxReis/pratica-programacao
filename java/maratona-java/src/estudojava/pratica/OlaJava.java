8f8ba3698a318f2e340242908f44f71492978a4c
Kevin Reis <kevinreis747@gmail.com>
2025-04-01 09:00:03 -0300

    Atualização automática - 2025-04-01 09:00:02

---
+++ java/maratona-java/src/estudojava/pratica/OlaJava.java
@@ -0,0 +1,13 @@
+package estudojava.pratica;
+
+public class OlaJava {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HAAAA");
+    }
+}
+// comentario use (//) para comentar
+
+/*
+    comentario de multiplas linhas
+    finalizar o comentario
+*/
