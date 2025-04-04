ac71b1c8baf532ea1581cfc03dacff45b9aae4ac
Kevin Reis <kevinreis747@gmail.com>
2025-04-04 19:44:31 -0300

    Atualização automática - 2025-04-04 19:44:30

--- java/maratona-java/src/estudojava/pratica/Ola2Java.java
+++ java/maratona-java/src/estudojava/pratica/Ola2Java.java
@@ -6,7 +6,7 @@ public class Ola2Java {
         int idade = 10; // -2.1e+9 a 2.1e+9
         long numeroGrande = 10000000000L; // -9.2e+18 a 9.2e+18
         double salarioDouble = 2550; // 1.7e -308 a 1.7e+308
-        float salarioFloat = 2500; // 3.4e -038 a 3.4e+038
+        float salarioFloat = 2500.0F; // 3.4e -038 a 3.4e+038
         byte idadeByte = 10; // -128 a 127
         short idadeShort = 20; // -32.768 a 32.767
         boolean verdadeiro = true; // true ou false
@@ -22,6 +22,20 @@ public class Ola2Java {
         System.out.println("O valor do long é " + numeroGrande);
         System.out.println("O valor do double é " + salarioDouble);
         System.out.println("O valor do float é " + salarioFloat);
+
+        // forçando o valor ser maior que o permitido
+        // fazendo o cast
+        int fteste = (int) 100000000000L;
+        float castsalario = (float) 2550.0D;
+        // cast nao é uma pratica recomendada
+
+        System.out.println(castsalario);
+        System.out.println(fteste);
+
+        // string é uma classe
+        String nome = "Kevin";
+        
+        System.out.println("Meu nome é "+nome);
 
     }
 }
