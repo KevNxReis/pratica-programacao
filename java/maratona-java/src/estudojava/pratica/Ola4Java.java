93599bee2faf6313afa5e5e8dea6725bcd3895bc
Kevin Reis <kevinreis747@gmail.com>
2025-04-08 08:00:02 -0300

    Atualização automática - 2025-04-08 08:00:02

--- java/maratona-java/src/estudojava/pratica/Ola4Java.java
+++ java/maratona-java/src/estudojava/pratica/Ola4Java.java
@@ -9,30 +9,28 @@ public class Ola4Java {
         System.out.println("Resultado da soma: " + resultado);
         System.out.println();
 
-        //%
         int resto = 20 % 2;
         System.out.println("Resto da divisão: " + resto);
         System.out.println();
 
-        // < > , <= >= , == (comparando), != (diferente)
         boolean isDezMaiorQueVinte = 10 > 20;
         boolean isDezMenorQueVinte = 10 < 20;
-        boolean isDezMaiorOuIgualQueVinte = 10 == 20;
-        boolean isDezMenorOuIgualQueVinte = 10 <= 20;
         boolean isDezDiferenteDeVinte = 10 != 20;
         System.out.println("É menor que: " + isDezMenorQueVinte);
         System.out.println("É maior que: " + isDezMaiorQueVinte);
-        System.out.println("É menor ou igual que: " + isDezMenorOuIgualQueVinte);
-        System.out.println("É maior ou igual que: " + isDezMaiorOuIgualQueVinte);
         System.out.println("É diferente de: " + isDezDiferenteDeVinte);
         System.out.println();
 
-        // && (e) || (ou) ! (não)
         boolean isDezMaiorQueVinteEIgualADez = (10 > 20) && (10 == 10);
         boolean isDezMaiorQueVinteOuIgualADez = (10 > 20) || (10 == 10);
-        boolean isDezMaiorQueVinteOuIgualADezNegado = !(10 > 20) || (10 == 10);
         System.out.println("É maior que e igual a: " + isDezMaiorQueVinteEIgualADez);
         System.out.println("É maior que ou igual a: " + isDezMaiorQueVinteOuIgualADez);
-        System.out.println("É maior que ou igual a negado: " + isDezMaiorQueVinteOuIgualADezNegado);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        double valorPrecoPlaystation5 = 5000; 
+        boolean isPlaystation5Compravel = valorTotalContaCorrente > valorPrecoPlaystation5
+                || valorTotalContaPoupanca > valorPrecoPlaystation5;
+        System.out.println("É maior que ou igual a: " + isPlaystation5Compravel);
     }
 }
