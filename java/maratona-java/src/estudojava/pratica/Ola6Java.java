fedb8712ee23f625bce674cbfc4d86f6a5b8f801
Kevin Reis <kevinreis747@gmail.com>
2025-04-29 20:00:02 -0300

    Atualização automática - 2025-04-29 20:00:02

--- java/maratona-java/src/estudojava/pratica/Ola6Java.java
+++ java/maratona-java/src/estudojava/pratica/Ola6Java.java
@@ -2,6 +2,22 @@ package estudojava.pratica;
 
 public class Ola6Java {
     public static void main(String[] args) {
+        int count = 0;
+        while(count < 10){
+            System.out.println(count);
+            count = count +1;
+        }
+        for (int valor = 0;valor < 10; valor ++) {
+            System.out.println("for" + valor);
+        }
+
+
+
+
+
+
+
+
 
     }
 }
