c925a165950f63588be1a698aa3ea8739dbf0b0d
Kevin Reis <kevinreis747@gmail.com>
2025-05-06 10:00:02 -0300

    Atualização automática - 2025-05-06 10:00:02

--- java/maratona-java/src/estudojava/pratica/Ola7Java.java
+++ java/maratona-java/src/estudojava/pratica/Ola7Java.java
@@ -2,6 +2,12 @@ package estudojava.pratica;
 
 public class Ola7Java {
     public static void main(String[] args) {
-        
+        int [] idades = new int[3];
+        idades[0] = 18;
+        idades[1] = 20;
+        idades[2] = 30;
+        System.out.println(idades[1]);
+        System.out.println(idades[0]);
+        System.out.println(idades[2]);
     }
 }
---
+++ java/maratona-java/src/estudojava/pratica/Ola7Java2.java
@@ -0,0 +1,13 @@
+package estudojava.pratica;
+
+public class Ola7Java2 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float, e double 0
+        // char '\u0000' ' '
+        // boolean false
+        // String null
+
+        
+
+    }
+}
