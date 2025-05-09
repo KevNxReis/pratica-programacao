c4983c06d3a85a89ff1f394db89faf3e6d717cea
Kevin Reis <kevinreis747@gmail.com>
2025-05-09 14:00:01 -0300

    Atualização automática - 2025-05-09 14:00:01

--- java/beecrowd/Exerc5.java
+++ java/beecrowd/Exerc5.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 
 public class Exerc5 {
     public static void main(String[] args) {
-        DecimalFormat decimalFormat = new DecimalFormat("#.#####");
+        DecimalFormat decimalFormat = new DecimalFormat("0.00000");
         Scanner scanner = new Scanner(System.in);
-        double A = scanner.nextDouble();
-        double B = scanner.nextDouble();
-        double soma = A + B;
-        double peso = soma / 2;
-        System.out.println("MEDIA = "+decimalFormat.format(peso));
+        double A = 0;
+        double B = 0;
+        boolean validar = false;
+
+        while (!validar) {
+            A = scanner.nextDouble();
+            B = scanner.nextDouble();
+
+            if ((A >= 0 && A <= 10.0) && (B >= 0 && B <= 10.0)) {
+                validar = true; // Sai do loop quando os valores são válidos
+            } else {
+                System.out.println("Coloque o valor de nota certa aí, animal");
+            }
+        }
+
+        double media = (A + B) / 2;
+        System.out.println("MEDIA = " + decimalFormat.format(media));
         scanner.close();
-
-
-
     }
 }
