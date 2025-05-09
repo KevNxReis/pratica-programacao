a15c058e22244ca638cdd97d1a2c52a5d80aae88
Kevin Reis <kevinreis747@gmail.com>
2025-05-08 21:00:01 -0300

    Atualização automática - 2025-05-08 21:00:01

--- java/beecrowd/Exerc2.java
+++ java/beecrowd/Exerc2.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Exerc2 {
     public static void main(String[] args) {
-        int A = 10;
-        int B = 9;
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
         int X = A + B;
         System.out.println("X = " + X );
+        scanner.close();
     }
 }
