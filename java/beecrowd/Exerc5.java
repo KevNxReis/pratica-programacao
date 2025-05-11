490b917bdb9c3f9cd3d6cf5c210611eb03efbc34
Kevin Reis <kevinreis747@gmail.com>
2025-05-11 08:00:02 -0300

    Atualização automática - 2025-05-11 08:00:02

--- java/beecrowd/Exerc5.java
+++ java/beecrowd/Exerc5.java
@@ -5,8 +5,8 @@ public class Exerc5 {
     public static void main(String[] args) {
         DecimalFormat decimalFormat = new DecimalFormat("0.00000");
         Scanner scanner = new Scanner(System.in);
-        double A = 0;
-        double B = 0;
+        double A = 0.0;
+        double B = 0.0;
         boolean validar = false;
 
         while (!validar) {
@@ -14,7 +14,8 @@ public class Exerc5 {
             B = scanner.nextDouble();
 
             if ((A >= 0 && A <= 10.0) && (B >= 0 && B <= 10.0)) {
-                validar = true; // Sai do loop quando os valores são válidos
+                validar = true;
+                break;
             } else {
                 System.out.println("Coloque o valor de nota certa aí, animal");
             }
