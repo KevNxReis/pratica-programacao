caea1205ffa137c38995317119046ab886de0eb7
Kevin Reis <kevinreis747@gmail.com>
2025-05-12 20:00:02 -0300

    Atualização automática - 2025-05-12 20:00:02

--- java/beecrowd/Exerc5.java
+++ java/beecrowd/Exerc5.java
@@ -5,25 +5,11 @@ public class Exerc5 {
     public static void main(String[] args) {
         DecimalFormat decimalFormat = new DecimalFormat("0.00000");
         Scanner scanner = new Scanner(System.in);
-        double A = 0.0;
-        double B = 0.0;
-        boolean validar = false;
-
-        while (!validar) {
-            System.out.println("insira o valor da nota A e em seguida da nota B:");
-            A = scanner.nextDouble();
-            B = scanner.nextDouble();
-
-            if ((A >= 0 && A <= 10.0) && (B >= 0 && B <= 10.0)) {
-                validar = true;
-            } else {
-                System.out.println("Coloque o valor de nota certa aí, animal");
-            }
-        }
-        System.out.println("insira o peso da nota A e em seguida da nota B:");
-        double pesoA = scanner.nextDouble();
-        double pesoB = scanner.nextDouble();
-        double multpeso = ((A * pesoA)+ (B * pesoB));
+        double notaA = scanner.nextDouble();
+        double notaB = scanner.nextDouble();
+        double pesoA = 3.5;
+        double pesoB = 7.5;
+        double multpeso = ((notaA * pesoA)+ (notaB * pesoB));
         double media = (multpeso/(pesoA+pesoB));
         System.out.println("MEDIA = " + decimalFormat.format(media));
         scanner.close();
