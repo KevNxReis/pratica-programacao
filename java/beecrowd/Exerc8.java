b334d2934f3b22cff2dd638f733a790d86d4cae0
Kevin Reis <kevinreis747@gmail.com>
2025-05-13 09:37:49 -0300

    Atualização automática - 2025-05-13 09:37:49

--- java/beecrowd/Exerc8.java
+++ java/beecrowd/Exerc8.java
@@ -1,5 +1,17 @@
+
+import java.text.DecimalFormat;
+
 public class Exerc8 {
     public static void main(String[] args) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        int numFuncionario = 25;
+        int dinheiro = 100;
+        double horas = 5.50;
+        double salario = dinheiro * horas;
+        System.out.println("NUMBER = "+numFuncionario);
+        System.out.println("SALARY = U$ "+decimalFormat.format(salario));
+
+
 
     }
 }
