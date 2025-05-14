df949819976c8a81617b2251184ecff04692cedf
Kevin Reis <kevinreis747@gmail.com>
2025-05-14 12:00:02 -0300

    Atualização automática - 2025-05-14 12:00:02

--- java/beecrowd/Exerc20.java
+++ java/beecrowd/Exerc20.java
@@ -3,12 +3,16 @@ public class Exerc20 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int idadedias = scanner.nextInt();
+
         int ano = idadedias / 365;
-        int mes = idadedias % 365 / 30;
-        int dia = idadedias % 30;
+        idadedias = idadedias % 365;
+        int mes = idadedias / 30;
+        idadedias = idadedias % 30;
+        int dia = idadedias;
+
         System.out.println(ano+" ano(s)"+"\n"
                 +mes+" mes(es)"+"\n"
-                +dia+" dia(es)");
+                +dia+" dia(s)");
         scanner.close();
 
     }
