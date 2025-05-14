3f5d73864b81b057b48cf71bf9ee68af1c792fcc
Kevin Reis <kevinreis747@gmail.com>
2025-05-14 08:00:02 -0300

    Atualização automática - 2025-05-14 08:00:02

--- java/beecrowd/Exerc16.java
+++ java/beecrowd/Exerc16.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Exerc16 {
     public static void main(String[] args) {
-        int X =;
-        int Y =;
+        Scanner scanner = new Scanner(System.in);
+        int Xkm = 60;
+        int Ykm = 90;
+        
+        System.out.println(distancia);
+
 
     }
 }
