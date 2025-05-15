1c10475ff5e382b654b5bb31027bdede2241ff06
Kevin Reis <kevinreis747@gmail.com>
2025-05-15 14:00:02 -0300

    Atualização automática - 2025-05-15 14:00:02

--- java/beecrowd/Exerc36.java
+++ java/beecrowd/Exerc36.java
@@ -8,9 +8,18 @@ public class Exerc36 {
         double A = scanner.nextDouble();
         double B = scanner.nextDouble();
         double C = scanner.nextDouble();
+        boolean c1 = A != 0;
+        double delta = Math.pow(B,2) -4*A*C;
+        boolean c2 = delta >= 0;
+        if (c1 && c2){
+            double x1 = (-B + Math.sqrt(delta)) / (2*A);
+            double x2 = (-B - Math.sqrt(delta)) / (2*A);
+            System.out.println("R1 = "+decimalFormat.format(x1)+"\n"+"R2 = "+decimalFormat.format(x2));
 
-        
-
+        }else{
+            System.out.println("Impossivel calcular");
+        }
+        scanner.close();
 
     }
 }
