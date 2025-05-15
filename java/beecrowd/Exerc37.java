bb687cd1fbcfeaec17aca112f84120a7affb884b
Kevin Reis <kevinreis747@gmail.com>
2025-05-15 15:00:02 -0300

    Atualização automática - 2025-05-15 15:00:02

--- java/beecrowd/Exerc37.java
+++ java/beecrowd/Exerc37.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Exerc37 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double valor = scanner.nextDouble();
+        
 
 
     }
