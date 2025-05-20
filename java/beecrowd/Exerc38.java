3eca6e701b037e4774c6e2426e86c23ddc5a130d
Kevin Reis <kevinreis747@gmail.com>
2025-05-20 13:42:31 -0300

    Atualização automática - 2025-05-20 13:42:31

--- java/beecrowd/Exerc38.java
+++ java/beecrowd/Exerc38.java
@@ -5,23 +5,17 @@ public class Exerc38 {
     public static void main(String[] args) {
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
         Scanner scanner = new Scanner(System.in);
-
-        // Array com os preços dos lanches
         double[] precos = {0.0, 4.00, 4.50, 5.00, 2.00, 1.50};
-
         System.out.println("Digite o código do lanche (1-5):");
         int codigo = scanner.nextInt();
-
         if (codigo >= 1 && codigo <= 5) {
             System.out.println("Digite a quantidade:");
             int quantidade = scanner.nextInt();
-
             double total = precos[codigo] * quantidade;
             System.out.println("Total: R$ " + decimalFormat.format(total));
         } else {
             System.out.println("Código inválido!");
         }
-
         scanner.close();
     }
 }
---
+++ java/beecrowd/Exerc40.java
@@ -0,0 +1,5 @@
+public class Exerc40 {
+    public static void main(String[] args) {
+
+    }
+}
