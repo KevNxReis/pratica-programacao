6e13e49a45957d09819b4df3bf955b79812dd914
Kevin Reis <kevinreis747@gmail.com>
2025-05-22 20:00:02 -0300

    Atualização automática - 2025-05-22 20:00:02

--- java/beecrowd/Exerc42.java
+++ java/beecrowd/Exerc42.java
@@ -3,7 +3,26 @@ import java.util.Scanner;
 public class Exerc42 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int a = num1;
+        int b = num2;
+        int c = num3;
+        if (a > b){
+            a = num2;
+            b = num1;
+        }
+        if (b > c){
+            b = num3;
+            c = num2;
+        }
+        if (a > b){
+            a = num3;
+            b = num2;
+        }
+        System.out.println(a+" "+b+" "+c);
+
 
 
     }
