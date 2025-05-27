740c3410a74f5cca989a6afb033df71eaaf507f3
Kevin Reis <kevinreis747@gmail.com>
2025-05-27 17:00:02 -0300

    Atualização automática - 2025-05-27 17:00:02

---
+++ java/beecrowd/Exerc46.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exerc46 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int horainicio1 = sc.nextInt();
+        int horafinal = sc.nextInt();
+        int dia = 24;
+        
+    }
+}
