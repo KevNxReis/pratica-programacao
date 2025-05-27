694cdc5377073f487d49b6f1f6c81ef55018b908
Kevin Reis <kevinreis747@gmail.com>
2025-05-27 11:00:02 -0300

    Atualização automática - 2025-05-27 11:00:02

--- java/beecrowd/Exerc45.java
+++ java/beecrowd/Exerc45.java
@@ -1,50 +1,36 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Exerc45 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double n1 = sc.nextDouble();
-        double n2 = sc.nextDouble();
-        double n3 = sc.nextDouble();
-        double A = n1;
-        double B = n2;
-        double C = n3;
-        double temp;
-        if (0<A && 0<B && 0<C) {
-        }else{
-
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+        if (!(A>0 && B>0 && C>0)) {
+            System.exit(1);
         }
-        if (A < B) {
-            temp = B;
-            B = A;
-            A = temp;
-        }
-        if (B < C) {
-            temp = C;
-            C = B;
-            B = temp;
-        }
-        if (C < A) {
-            temp = A;
-            A = C;
-            C = temp;
-        }
+        Double [] valores = {A,B,C};
+        Arrays.sort(valores, Collections.reverseOrder());
+        A = valores[0];
+        B = valores[1];
+        C = valores[2];
         if (A >= (B + C)) {
             System.out.println("NAO FORMA TRIANGULO");
+            System.exit(1);
         }
         double a1 = Math.pow(A, 2);
         double b1 = Math.pow(B, 2);
         double c1 = Math.pow(C, 2);
         if (a1 == (b1 + c1)) {
             System.out.println("TRIANGULO RETANGULO");
-        }
-        if (a1 > (b1 + c1)) {
+        }else if (a1 > (b1 + c1)) {
             System.out.println("TRIANGULO OBTUSANGULO");
-        }
-        if (a1 < (b1 + c1)) {
+        }else if (a1 < (b1 + c1)) {
             System.out.println("TRIANGULO ACUTANGULO");
         }
-        if (A == B && B == C && C == A) {
+        if (A == B && B == C ) {
             System.out.println("TRIANGULO EQUILATERO");
         }
         if (A != B && B == C && C != A || A != B && B != C && C == A ||
