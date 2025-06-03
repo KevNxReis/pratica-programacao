7b0b9d07e0c322f3e5bfbb9bd9e663dd2107751b
Kevin Reis <kevinreis747@gmail.com>
2025-06-03 19:00:02 -0300

    Atualização automática - 2025-06-03 19:00:02

--- python/CursoSantander/Aula1.py
+++ python/CursoSantander/Aula1.py
@@ -1,1 +1,17 @@
 print ("Olá, mundo!")
+
+lista = ['cachorro','macaco','gato']
+#print(lista[0])
+#print(lista[2])
+#print(lista[-2])
+
+#remove_lista = lista.pop(0)
+#print(lista)
+#print(remove_lista)
+
+lista.sort()
+print(lista)
+
+lista.reverse()
+print(lista)
+
