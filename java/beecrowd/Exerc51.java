193bd81b33d763de46facc41abe06d006dd06a76
Kevin Reis <kevinreis747@gmail.com>
2025-06-06 19:00:02 -0300

    Atualização automática - 2025-06-06 19:00:02

--- java/beecrowd/Exerc51.java
+++ java/beecrowd/Exerc51.java
@@ -6,15 +6,21 @@ public class Exerc51 {
         Scanner scanner = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.00");
         double salario = scanner.nextDouble();
+        double imposto1 = 80.00;
+        double imposto2 = 270.00;
         switch (salario) {
-            case salario>0.00 && salario<2000.00:
+            case salario>=0.00 && salario<=2000.00:
                 System.out.println("Isento");
                 break;
-            case salario>2000.01 && salario<3000.00:
+            case salario>=2000.01 && salario<=3000.00:
                 calculo = salario;
                 System.out.println(calculo);
                 break;
-            case salario>3000.01 && salario<4500.00:
+            case salario>=3000.01 && salario<=4500.00:
+                System.out.println(calculo);
+                break;
+            case salario>=4500.01:
+                calculo = salario;
                 System.out.println(calculo);
                 break;
         }
