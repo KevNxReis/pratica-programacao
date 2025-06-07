e17f5f7d66fe2a260f4c1e37f8f4a1f99791d883
Kevin Reis <kevinreis747@gmail.com>
2025-06-06 22:00:02 -0300

    Atualização automática - 2025-06-06 22:00:02

--- java/beecrowd/Exerc51.java
+++ java/beecrowd/Exerc51.java
@@ -8,21 +8,24 @@ public class Exerc51 {
         double salario = scanner.nextDouble();
         double imposto1 = 80.00;
         double imposto2 = 270.00;
-        switch (salario) {
-            case salario>=0.00 && salario<=2000.00:
-                System.out.println("Isento");
-                break;
-            case salario>=2000.01 && salario<=3000.00:
-                calculo = salario;
-                System.out.println(calculo);
-                break;
-            case salario>=3000.01 && salario<=4500.00:
-                System.out.println(calculo);
-                break;
-            case salario>=4500.01:
-                calculo = salario;
-                System.out.println(calculo);
-                break;
+        double calculo = 0;
+        double imposto = 0;
+
+        if (salario >= 0.00 && salario <= 2000.00) {
+            System.out.println("Isento");
+        } else if (salario >= 2000.01 && salario <= 3000.00) {
+            calculo = (salario - 2000.00) * 0.08;
+            System.out.println("R$ " + df.format(calculo));
+        } else if (salario >= 3000.01 && salario <= 4500.00) {
+            calculo = (salario - 3000.00) * 0.18;
+            double valor = calculo + imposto1;
+            System.out.println("R$ " + df.format(valor));
+        } else if (salario > 4500.00) {
+            calculo = (salario - 4500.00) * 0.28;
+            imposto = calculo + imposto1 + imposto2;
+            System.out.println("R$ " + df.format(imposto));
         }
+
+        scanner.close();
     }
 }
