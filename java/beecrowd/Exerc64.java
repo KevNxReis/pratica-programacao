7bbdf8ef9b92ae85a6f909493e8ad8212e1d7877
Kevin Reis <kevinreis747@gmail.com>
2025-06-17 16:00:02 -0300

    Atualização automática - 2025-06-17 16:00:02

--- java/beecrowd/Exerc64.java
+++ java/beecrowd/Exerc64.java
@@ -1,8 +1,33 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Exerc64 {
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("0.0");
         Scanner sc = new Scanner(System.in);
-        double 
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        double num3 = sc.nextDouble();
+        double num4 = sc.nextDouble();
+        double num5 = sc.nextDouble();
+        double num6 = sc.nextDouble();
+        double[] nums = {num1, num2, num3, num4, num5, num6};
+        int positivos = 0;
+        double soma = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                positivos++;
+                soma += nums[i];
+            }
+        }
+
+        System.out.println(positivos + " valores positivos");
+        if (positivos > 0) {
+            double media = soma / positivos;
+            System.out.println(df.format(media));
+        }
+
+        sc.close();
     }
 }
