818848d8c8f1ef6b5006f05a21ce8543f0b60c5f
Kevin Reis <kevinreis747@gmail.com>
2025-08-06 15:00:02 -0300

    Atualização automática - 2025-08-06 15:00:02

--- curso_gedaia/exerc2.py
+++ curso_gedaia/exerc2.py
@@ -3,4 +3,19 @@ import sys
 def soma():
     try:
         n = int(input())
-        lista
+        lista = input()
+        list_num = lista.split()
+        valor = []
+        for numer in list_num:
+            valor.append(int(numer))
+
+        total_soma = sum(valor)
+
+        print(total_soma)
+
+    except (ValueError, IOError):
+        print("ta errado meu mano")
+
+soma()
+
+
