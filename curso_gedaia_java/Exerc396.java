63f1fd8741fb49c49836cf4019edf824e8ab62f9
Kevin Reis <kevinreis747@gmail.com>
2025-09-04 16:00:02 -0300

    Atualização automática - 2025-09-04 16:00:02

--- curso_gedaia_java/Exerc396.java
+++ curso_gedaia_java/Exerc396.java
@@ -1,7 +1,14 @@
 public class Exerc396 {
     public static void main(String[] args) {
         int[] nums = {4,3,2,6};
+
         for (int i = 0; i < nums.length; i++) {
+            int F = (0 * nums[0]) + (1 * nums[1]) + (2 * nums[2]) + (3 * nums[3]);
+            int startIndex = 0 ;
+
+            for (int j = startIndex; j <nums.length; j++ ){
+
+            }
 
         }
 
