793d8c2b0e44cb872b3c7b26328fdbb9fd09286a
Kevin Reis <kevinreis747@gmail.com>
2025-09-18 17:00:02 -0300

    Atualização automática - 2025-09-18 17:00:02

---
+++ curso_gedaia_java/Exerc90.java
@@ -0,0 +1,11 @@
+public class Exerc90 {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 0; i < (1 << n); i ++){
+            System.out.println(Integer.toBinaryString(i));
+        }
+    }
+}
+
+// Manipulação de bits
+//
